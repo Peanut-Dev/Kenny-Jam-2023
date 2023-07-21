71b25eb76d512e0ae3eda4e2ecb90676ca0293dd
Peanut <63982558+69ShadesOfPeanut@users.noreply.github.com>
2023-07-22 01:20:33 +1000

    Grid based movement system

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,13 +1,27 @@
 extends CharacterBody2D
 
+# Vars
+# Export vars
 @export var speed: int = 0
+@export var tileSize = 16
+# Other
+var inputs = {"right": Vector2.RIGHT,
+			"left": Vector2.LEFT,
+			"up": Vector2.UP,
+			"down": Vector2.DOWN}
 
-# get the keys the player is facing then turns it into directional velocity
-func get_input():
-	var inputDirection: Vector2 = Input.get_vector("left", "right", "up", "down");
-	velocity = inputDirection * speed;
 
-# calls get input and handles moving the player
-func _physics_process(delta):
-	get_input();
-	move_and_slide();
+# On ready, snap player to grid
+func _ready():
+	position = position.snapped(Vector2.ONE * tileSize)
+	position += Vector2.ONE * tileSize/2
+
+# If player presses movement key, trigger move func
+func _unhandled_input(event):
+	for dir in inputs.keys():
+		if event.is_action_pressed(dir):
+			move(dir)
+
+# Moves the player based on the tilemap and input
+func move(dir):
+	position += inputs[dir] * tileSize
