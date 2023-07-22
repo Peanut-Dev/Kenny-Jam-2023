70f55782a2b5ad21b6642847cfeecc9576624363
Peanut <63982558+69ShadesOfPeanut@users.noreply.github.com>
2023-07-23 02:39:41 +1000

    Basics of job system

---
+++ Scripts/Data/JobData.gd
@@ -0,0 +1,23 @@
+# Script for handling the job data
+extends Node
+
+var currentJob : int;
+var itemData = {};
+var dataFilePath = "res://Data/Jobs.json";
+
+
+func _ready():
+	itemData = loadJsonFile(dataFilePath);
+
+# Loads a json file and returns the parsed results
+func loadJsonFile(filePath : String):
+	if FileAccess.file_exists(filePath):
+		var dataFile = FileAccess.open(filePath, FileAccess.READ);
+		var parsedResult = JSON.parse_string(dataFile.get_as_text());
+		
+		if parsedResult is Dictionary:
+			return parsedResult;
+		else:
+			print("Error reading file");
+	else:
+		print("File path does not exist!");
--- Scripts/Interactable/JobBoard.gd
+++ Scripts/Interactable/JobBoard.gd
@@ -2,11 +2,27 @@
 extends Interactable
 
 # Vars
+# Nodes
 @onready var jobListingsUI : Control = get_node("%JobListingsUI");
+@onready var jobListButtons : VBoxContainer = jobListingsUI.get_node("%JobListButtons")
 
 # Constantly checks if the player is in range and presses F
+# Sets the job button names in the job listing UI
 func _process(delta):
 	if isInRange == true and Input.is_action_just_pressed("interact"):
+		get_tree().paused = !get_tree().paused
+		
 		print("Player interacted with " + name);
 		
 		jobListingsUI.visible = !jobListingsUI.visible;
+		
+		var i = 0
+		for button in jobListButtons.get_children():
+			print(button.name)
+			print(JobData.itemData["Jobs"][i])
+			
+			button.set_text(JobData.itemData["Jobs"][i]["Name"])
+			
+			i += 1
+		
+		jobListButtons.get_child(0).grab_focus()
