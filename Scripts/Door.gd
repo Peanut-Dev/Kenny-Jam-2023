5d4983c9c7375e160a65bcbbf34346087e5ea483
Peanut <63982558+69ShadesOfPeanut@users.noreply.github.com>
2023-07-23 22:14:44 +1000

    mmm yea
    stops player from entering forest without a job

--- Scripts/Door.gd
+++ Scripts/Door.gd
@@ -5,12 +5,17 @@ extends Area2D
 # Vars
 # Export vars
 @export_file("*.tscn") var scene;
+@export var isForestDoor : bool;
 
 
 # On body entering, check if its the player.
 # If its the player, change to the next scene saved in the scene var
 func onBodyEntered(body : CharacterBody2D):
 	if body.name == "Player":
+		if isForestDoor == true and JobData.currentJob == 0:
+			print("Player does not have job yet!")
+			return
+		
 		print("Attempting to change scene...");
 		print(scene);
 		
