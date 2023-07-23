c85618084acee705d75322cb30ab3389046278fc
Peanut <63982558+69ShadesOfPeanut@users.noreply.github.com>
2023-07-23 22:09:13 +1000

    Job descriptions

--- Scripts/Interactable/JobBoard.gd
+++ Scripts/Interactable/JobBoard.gd
@@ -4,13 +4,13 @@ extends Interactable
 # Vars
 # Nodes
 @onready var jobListingsUI : Control = get_node("%JobListingsUI");
-@onready var jobListButtons : VBoxContainer = jobListingsUI.get_node("%JobListButtons")
+@onready var jobListButtons : VBoxContainer = jobListingsUI.get_node("%JobListButtons");
 
 # Constantly checks if the player is in range and presses F
 # Sets the job button names in the job listing UI
 func _process(delta):
 	if isInRange == true and Input.is_action_just_pressed("interact"):
-		get_tree().paused = !get_tree().paused
+		get_tree().paused = !get_tree().paused;
 		
 		print("Player interacted with " + name);
 		
@@ -18,11 +18,24 @@ func _process(delta):
 		
 		var i = 0
 		for button in jobListButtons.get_children():
-			print(button.name)
-			print(JobData.itemData["Jobs"][i])
+			print(button.name);
+			print(JobData.itemData["Jobs"][i]);
 			
-			button.set_text(JobData.itemData["Jobs"][i]["Name"])
+			button.set_text(JobData.itemData["Jobs"][i]["Name"]);
 			
-			i += 1
+			button.set_meta("jobID", JobData.itemData["Jobs"][i]["ID"]);
+			
+			button.pressed.connect(jobSelected);
+			print("Signal connected");
+			
+			i += 1;
 		
-		jobListButtons.get_child(0).grab_focus()
+		jobListButtons.get_child(0).grab_focus();
+
+# When a button is pressed, save the selected job
+func jobSelected():
+	for button in jobListButtons.get_children():
+		if button.is_pressed() == true:
+			JobData.currentJob = button.get_meta("jobID");
+	
+	print("Job selected");
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 # Vars
 # Nodes
+@onready var jobDescriptionUI : Control = get_node("%JobDescription")
 @onready var ray : RayCast2D = $RayCast2D
 @onready var runningTimer : Timer = $RunningTimer
 # Export vars
@@ -26,6 +27,14 @@ func _physics_process(delta):
 	if Input.is_action_pressed("right") and runningTimer.is_stopped() == true:
 		move(Vector2.RIGHT);
 		runningTimer.start()
+	
+	if Input.is_action_just_pressed("JobInfo"):
+		if JobData.currentJob == 0:
+			return
+		
+		jobDescriptionUI.visible = !jobDescriptionUI.visible
+		
+		jobDescriptionUI.get_node("%DescriptionLabel").set_text(JobData.itemData["Jobs"][JobData.currentJob - 1]["Description"])
 
 # Check if the player is about to make an illegal move (Collision)
 # If move is legal, move the player by one tile
